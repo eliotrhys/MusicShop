74561be1a3ec5196707c3b7964bfad545ee82175
eliotrhys <eliot@eliotshort.co.uk>
2017-12-17 16:21:09 +0000

    multiple types added - finished

---
+++ src/main/java/GuitarPick.java
@@ -0,0 +1,9 @@
+public class GuitarPick extends Item {
+
+    public GuitarPick(String item, double buyPrice, double sellPrice){
+        this.item = item;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+    }
+
+}
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -8,12 +8,14 @@ public class ShopTest {
     Shop testShop;
     Guitar testGuitar;
     SheetMusic testSheetMusic;
+    GuitarPick testGuitarPick;
 
     @Before
     public void before(){
         testGuitar = new Guitar("Gibson", "Hummingbird", 1299.99, 1799.99, "Wood");
         testSheetMusic = new SheetMusic("Moonlight Sonata", 9.99, 16.99);
         testShop = new Shop();
+        testGuitarPick = new GuitarPick("Dunlop Pick", 0.50, 1.50);
     }
 
     @Test
@@ -36,6 +38,14 @@ public class ShopTest {
         testShop.addToStock(testGuitar);
         testShop.addToStock(testSheetMusic);
         assertEquals(testShop.calculateTotalProfit(), 507, 0.2);
+    }
+
+    @Test
+    public void canAddMultipleTypes(){
+        testShop.addToStock(testGuitar);
+        testShop.addToStock(testSheetMusic);
+        testShop.addToStock(testGuitarPick);
+        assertEquals(testShop.stockCount(), 3);
     }
 
 }
